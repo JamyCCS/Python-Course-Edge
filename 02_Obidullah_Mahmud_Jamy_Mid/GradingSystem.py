64685763ad691cc9fa3067b59a27c7ccd4360c6c
JamyCCS <obidullahjamy@gmail.com>
2025-01-01 18:33:01 +0600

    Completed qustion 2

--- 02_Obidullah_Mahmud_Jamy_Mid/GradingSystem.py
+++ 02_Obidullah_Mahmud_Jamy_Mid/GradingSystem.py
@@ -5,12 +5,15 @@ def calculate_grade(score):
         return 'B'
     elif 70 <= score < 80:
         return 'C'
+    elif score>100:
+        return 'Invalid Score'
     else:
         return 'F'
+    
 
-def class_performance(scores):
-    total_score = sum(scores)
-    average_score = total_score / len(scores)
+def class_performance(scores_dict):
+    total_score = sum(scores_dict.values())
+    average_score = total_score / len(scores_dict)
     
     if average_score > 85:
         performance = "Excellent Performance"
@@ -19,8 +22,22 @@ def class_performance(scores):
     else:
         performance = "Needs Improvement"
     
-    return average_score, performance
+    return average_score,performance
 
+
+n=int(input("Enter the number of students: "))
+
+resultbook = {}
+
+for i in range(n):
+    student_score = int(input("Enter student score: "))
+    grade = calculate_grade(student_score)
+    print(f"Grade is {grade}\n")
+    resultbook[i] = student_score
+
+
+average_score, performance = class_performance(resultbook)
+print(f"The class Average is {average_score} and the class performance is {performance}")
 
 
     
